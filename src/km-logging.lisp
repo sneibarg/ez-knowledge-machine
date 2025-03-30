95c91bc89b1d0c6d646a8bd37087e0389e1b3ff1
Scott Neibarger <scott.neibarger@gmail.com>
2025-03-30 15:48:59 -0400

    Added log4cl.

--- src/km-logging.lisp
+++ src/km-logging.lisp
@@ -8,6 +8,6 @@
   "Set up the root logger to log messages to the specified file with the given log level.
    FILENAME is the path to the log file.
    LEVEL can be a keyword such as :debug, :info (default), :warn, or :error."
-  (let ((appender (make-instance 'log4cl:file-appender)))
+  (let ((appender (make-instance 'log4cl:file-appender :file filename)))
     (log4cl:add-appender log4cl:*root-logger* appender)
     (log4cl:set-log-level log4cl:*root-logger* level)))
